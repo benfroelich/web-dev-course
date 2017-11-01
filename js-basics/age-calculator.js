c1779dae13073c923e44e85a511a2091ccd51a8a
Benny <benfroelich@gmail.com>
2017-10-31 22:19:48 -0700

    wip javascript basics

---
+++ js-basics/age-calculator.js
@@ -0,0 +1,23 @@
+var birth_year = prompt("Please enter your birth year: ");
+var birth_month = prompt("Please enter your birth month: ");
+var birth_day = prompt("Please enter your birth day: ");
+var birthday = new Date(birth_year, birth_month - 1, birth_day);
+
+var days_alive = (Date.now() - birthday.getTime()) / 1000 / 3600 / 24;
+
+var now = new Date();
+// manually calculate the number of days alive
+var days_alive_manual = 0;
+var manual_years = now.getYear() - birthday.getYear();
+var manual_months = now.getMonth() - birthday.getMonth();
+var manual_days = now.getDate() - birthday.getDate();
+
+// add days for each year preceeding this one
+for (var year = birthday.getYear(); year > now.getYear(); year++) 
+{
+	// leap years have one less day (364) than a normal year (365)
+	var days_in_year = year % 4 ? 365 : 364;
+	days_alive_manual += days_in_year;
+}
+
+birthday.getYear()console.log("You've been alive for " + days_alive + " " + "days!");
---
+++ js-basics/script.js
@@ -0,0 +1,6 @@
+var user_first_name = prompt("first name:");
+var user_last_name = prompt("last name:");
+var user_age = prompt("age:");
+console.log("We just encountered " + user_first_name + 
+	" " + user_last_name);
+console.log("age: " + user_age);
